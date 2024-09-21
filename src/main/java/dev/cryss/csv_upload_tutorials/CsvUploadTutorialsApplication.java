970b234fa1e6c8c79e7f045d3c39a3e91b65830a
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-09-21 17:27:51 -0300

    initial commit

---
+++ src/main/java/dev/cryss/csv_upload_tutorials/CsvUploadTutorialsApplication.java
@@ -0,0 +1,13 @@
+package dev.cryss.csv_upload_tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvUploadTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvUploadTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cryss/csv_upload_tutorials/CsvUploadTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cryss.csv_upload_tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvUploadTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
